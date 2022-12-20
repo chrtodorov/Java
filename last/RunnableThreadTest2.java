8e1f82b58d8fa5b63f36b5c848993b7c0a334045
chrtodorov <65716909+chrtodorov@users.noreply.github.com>
2022-12-20 09:53:24 +0200

    Delete last directory

--- last/RunnableThreadTest2.java
+++
@@ -1,9 +0,0 @@
-
-public class RunnableThreadTest2 {
-
-    public static void main(String args[]) {
-        new PrintNameRunnable("A");
-        new PrintNameRunnable("B");
-        new PrintNameRunnable("C");
-    }
-}
